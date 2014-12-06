2fd7d6df874f54567d209fca83b9fa312dc12e2c
Jesse Schneider <jmschneider430@gmail.com>
2014-12-06 11:10:45 -0500

    minor changes

--- RSS reader app/RSSTableViewController.m
+++ RSS reader app/RSSTableViewController.m
@@ -2,7 +2,7 @@
 //  RSSTableViewController.m
 //  RSS reader app
 //
-//  Created by Kervins Valcourt on 11/9/14.
+//  Created by Jesse Schneider on 12/6/14.
 //  Copyright (c) 2014 EastoftheWestEnd. All rights reserved.
 //
 
--- RSS reader appTests/RSS_reader_appTests.m
+++ RSS reader appTests/RSS_reader_appTests.m
@@ -2,7 +2,7 @@
 //  RSS_reader_appTests.m
 //  RSS reader appTests
 //
-//  Created by Kervins Valcourt on 11/9/14.
+//  Created by Jesse Schneider on 12/6/14.
 //  Copyright (c) 2014 EastoftheWestEnd. All rights reserved.
 //
 
